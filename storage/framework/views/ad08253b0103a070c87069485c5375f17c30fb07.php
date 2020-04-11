8b502a5be152c3ee5e4449d3b86815a3dc266719
simantoahmd <36773037+simantoahmd@users.noreply.github.com>
2020-04-12 00:05:14 +0600

    Add files via upload

---
+++ storage/framework/views/ad08253b0103a070c87069485c5375f17c30fb07.php
@@ -0,0 +1,45 @@
+<?php $__env->startSection('title'); ?>
+     Dashboard | Funda Of Online Car Rental System
+<?php $__env->stopSection(); ?> 
+
+
+<?php $__env->startSection('content'); ?>
+<div class="row">
+          <div class="col-md-12">
+            <div class="card">
+              <div class="card-header">
+                <h4 class="card-title"> Simple Table</h4>
+              </div>
+              <div class="card-body">
+                <div class="table-responsive">
+                  <table class="table">
+                    <thead class=" text-primary">
+                      <th> Name </th>
+                      <th> Country </th>
+                      <th> City </th>
+                      <th> Salary </th>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td> Dakota Rice </td>
+                        <td> Dakota Rice </td>
+                        <td> Dakota Rice </td>
+                        <td> Dakota Rice </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+    </div>
+
+
+<?php $__env->stopSection(); ?>
+
+
+<?php $__env->startSection('scripts'); ?>
+
+
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.master', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\laravel\resources\views/admin/dashboard.blade.php ENDPATH**/ ?>
