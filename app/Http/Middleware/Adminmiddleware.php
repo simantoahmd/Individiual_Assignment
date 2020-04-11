dadbe0e88be7d5dd25aac504af509796cb62d20f
simantoahmd <36773037+simantoahmd@users.noreply.github.com>
2020-04-11 23:25:23 +0600

    Auth,providers,middleware

---
+++ app/Http/Middleware/Adminmiddleware.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+class Adminmiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {  
+        if(Auth::user()->user_type == 'admin')
+        {
+        
+        return $next($request);
+
+        }
+       else
+       {
+        
+        return redirect('/home')->with('status','You are not Allowed To Admin Dashboard');
+           
+       }
+
+
+
+        
+    }
+}
